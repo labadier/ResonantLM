fd82c7166e1f4729c11d335ec121f4c38be8bac5
labadier <rlabadiet@gmail.com>
2022-06-02 19:52:08 -0400

    added emolex data

--- utils/params.py
+++ utils/params.py
@@ -19,24 +19,15 @@ class params:
   EMBD_SIZE = 1024 #! TODO change to 1024
 
 class bcolors:
-  HEADER = ''
-  OKBLUE = ''
-  OKCYAN = ''
-  OKGREEN = ''
-  WARNING = ''
-  FAIL = ''
-  ENDC = ''
-  BOLD = ''
-  UNDERLINE = ''
-  # HEADER = '\033[95m'
-  # OKBLUE = '\033[94m'
-  # OKCYAN = '\033[96m'
-  # OKGREEN = '\033[92m'
-  # WARNING = '\033[93m'
-  # FAIL = '\033[91m'
-  # ENDC = '\033[0m'
-  # BOLD = '\033[1m'
-  # UNDERLINE = '\033[4m'
+  HEADER = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKCYAN = '\033[96m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
 
 class PPLM:
 
