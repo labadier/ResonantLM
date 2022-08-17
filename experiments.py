a831b40b3a5d1ffede19cc10a1e97cf4e1f7f308
labadier <rlabadiet@gmail.com>
2022-08-17 16:34:41 +0200

    added experiments

--- experiments.py
+++ experiments.py
@@ -1,11 +1,19 @@
 import os
-import numpy as np
 
 phrases = ["Women's Harley Davidson Jacket",
 "White pure linen curtain",
 "Restaurant LE CONFIDENTIAL MARRAKECH international meals",
 "Papaya body cream",
-"5 nights all inclusive hotel accommodation at Disneyland Paris"]
+"5 nights all inclusive hotel accommodation at Disneyland Paris", 
+"Natural cream", 
+"cleaning products"]
 
-for i in phrases:
-    os.system(f'python main_generation.py -seed "{i}" -dt agreeableness')
+
+gm_scales = [0.2, 0.4, 0.6, 0.8] 
+
+for p in ['O', 'C', 'E', 'A', 'N']:
+  print("========== Factea O ==============\n")
+  for gm in gm_scales:
+    print("=========== gm: {gm} =============\n")
+    for i in phrases:
+        os.system(f'python main_generation.py -seed "{i}" -dt {p} -gm {gm}')
