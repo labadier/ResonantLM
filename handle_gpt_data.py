ba306ee928724eee93457a9d32650fec9491e5a3
labadier <rlabadiet@gmail.com>
2022-09-19 14:54:57 +0200

    leadded dtaset

---
+++ handle_gpt_data.py
@@ -0,0 +1,30 @@
+#%%
+
+import json
+import pandas as pd, csv
+
+dirs = '../gpt-2-output-dataset/data/large-762M.test.jsonl'
+
+jsonObj = pd.read_json(path_or_buf=dirs, lines=True)
+#%%
+
+with open('data.csv', 'wt', newline='', encoding="utf-8") as csvfile:
+  
+  spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+  spamwriter.writerow(['text'])
+
+  for i in range(len(jsonObj)):
+    jsonObj['text'][i] = jsonObj['text'][i].replace('\n\n', '\n')
+    if jsonObj.iloc[i]['length'] > 250:
+      texts = jsonObj.iloc[i]['text'].split('\n')
+    else: texts = [jsonObj.iloc[i]['text']]
+
+    for text in texts:
+          spamwriter.writerow([text])
+
+# %%
+
+from datasets import load_dataset
+
+dataset = load_dataset("bookcorpus")
+# %%
